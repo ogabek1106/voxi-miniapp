c299064c9da56d6b7d59d75c0225ebfa2f8753b5
ogabek1106 <84424884+ogabek1106@users.noreply.github.com>
2026-02-10 17:50:47 +0300

    Update models.py

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,10 +1,11 @@
 #backend/app/models.py
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, BigInteger
+
 from .db import Base
 
 class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    telegram_id = Column(String, unique=True, index=True, nullable=False)
+    telegram_id = Column(BigInteger, unique=True, index=True, nullable=False)
     name = Column(String, nullable=False)
