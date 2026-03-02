63035e021d72282ac17e00ebf0e5eb3ecbe28c00
ogabek1106 <84424884+ogabek1106@users.noreply.github.com>
2026-03-02 17:44:15 +0300

    Update env.py

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -12,7 +12,6 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")
 from app.models import Base
 
 config = context.config
-fileConfig(config.config_file_name)
 
 target_metadata = Base.metadata
 
