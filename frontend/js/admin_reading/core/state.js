9e2416813689556dcee488475dbabf41b3fed35b
ogabek1106 <84424884+ogabek1106@users.noreply.github.com>
2026-03-09 11:45:34 +0300

    Update state.js

--- frontend/js/admin_reading/core/state.js
+++ frontend/js/admin_reading/core/state.js
@@ -1,1 +1,8 @@
+// frontend/js/admin_reading/core/state.js
+window.AdminReading = window.AdminReading || {};
 
+AdminReading.state = {
+    currentPackId: null,
+    currentEditingTestId: null,
+    globalQuestionCounter: 1
+};
