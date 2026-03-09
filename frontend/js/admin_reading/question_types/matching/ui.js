7fba319ebdef03b43401ab3425f48b6cb65c82c4
ogabek1106 <84424884+ogabek1106@users.noreply.github.com>
2026-03-09 13:52:59 +0300

    Create ui.js

---
+++ frontend/js/admin_reading/question_types/matching/ui.js
@@ -0,0 +1,20 @@
+// frontend/js/admin_reading/question_types/matching/ui.js
+window.AdminReading = window.AdminReading || {};
+
+AdminReading.renderMatchingMeta = function(wrap) {
+
+  wrap.innerHTML = `
+
+<label>How many questions</label>
+<input class="match-q-count" type="number" min="1" value="3" />
+
+<label style="margin-top:6px; display:block;">How many options</label>
+<input class="match-opt-count" type="number" min="2" value="5" />
+
+<div class="matching-editor" style="margin-top:10px;"></div>
+
+`;
+
+  AdminReading.matchingInit(wrap);
+
+};
