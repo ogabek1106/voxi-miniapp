4af5ffdd1b906412fe6eac7f3f5b36b4807522c1
ogabek1106 <84424884+ogabek1106@users.noreply.github.com>
2026-03-09 15:38:24 +0300

    Update loader.js

--- frontend/js/admin_reading/question_types/matching/loader.js
+++ frontend/js/admin_reading/question_types/matching/loader.js
@@ -1,14 +1,14 @@
 // frontend/js/admin_reading/question_types/matching/loader.js
 window.AdminReading = window.AdminReading || {};
 
-// loader for matching question type
+// register matching question type
 AdminReading.registerQuestionType("matching", function(container, data = null) {
 
-  if (!AdminReadingMatchingRender) {
-    console.error("Matching renderer not loaded");
+  if (!AdminReading.renderMatchingMeta) {
+    console.error("Matching UI not loaded");
     return;
   }
 
-  AdminReadingMatchingRender(container, data);
+  AdminReading.renderMatchingMeta(container);
 
 });
