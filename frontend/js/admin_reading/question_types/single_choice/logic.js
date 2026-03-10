20a95d0ca42fad3596eab8133ce54ff6a42c9287
ogabek1106 <84424884+ogabek1106@users.noreply.github.com>
2026-03-10 15:05:39 +0300

    Update logic.js

--- frontend/js/admin_reading/question_types/single_choice/logic.js
+++ frontend/js/admin_reading/question_types/single_choice/logic.js
@@ -1,1 +1,15 @@
+// frontend/js/admin_reading/question_types/single_choice/logic.js
+window.AdminReading = window.AdminReading || {};
 
+AdminReading.registerQuestionType("single_choice", function(container, data = null) {
+
+  console.log("MCQ loader triggered", container);
+
+  if (!AdminReading.renderSingleChoiceUI) {
+    console.error("renderSingleChoiceUI not found");
+    return;
+  }
+
+  AdminReading.renderSingleChoiceUI(container, data);
+
+});
