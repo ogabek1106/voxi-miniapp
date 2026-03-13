4c59b2fcdf890707fba06cede4804399f8608baf
ogabek1106 <84424884+ogabek1106@users.noreply.github.com>
2026-03-13 14:31:22 +0300

    Update render.js

--- frontend/js/admin_reading/question_types/single_choice/render.js
+++ frontend/js/admin_reading/question_types/single_choice/render.js
@@ -1,1 +1,13 @@
+// frontend/js/admin_reading/question_types/single_choice/render.js
+window.AdminReading = window.AdminReading || {};
 
+AdminReading.renderSingleChoice = function(container, data = null) {
+
+  if (!AdminReading.renderSingleChoiceUI) {
+    console.error("renderSingleChoiceUI not found");
+    return;
+  }
+
+  AdminReading.renderSingleChoiceUI(container, data);
+
+};
