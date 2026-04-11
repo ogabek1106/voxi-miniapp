a5d933169deca6718f70fddc16a93a71e0bdfa20
ogabek1106 <84424884+ogabek1106@users.noreply.github.com>
2026-04-11 17:07:06 +0300

    Update serializer.js

--- frontend/js/admin_reading/question_types/ynng/serializer.js
+++ frontend/js/admin_reading/question_types/ynng/serializer.js
@@ -1,18 +1,24 @@
 // frontend/js/admin_reading/question_types/ynng/serializer.js
+
 window.AdminReading = window.AdminReading || {};
 AdminReading.YNNG = AdminReading.YNNG || {};
 
-AdminReading.serializeYNNG = function(block) {
+AdminReading.serializeYNNG = function (block, orderIndex) {
 
   const text = block.querySelector(".ynng-question")?.value?.trim();
   const correct = block.querySelector(".ynng-correct")?.value;
+
+  const instruction = block.querySelector(".q-instruction-select")?.value?.trim() || null;
 
   if (!text) return null;
 
   return {
     type: "YES_NO_NG",
+    order_index: orderIndex,
+    instruction: instruction, // ✅ FIX
     content: { text },
     correct_answer: { value: correct },
-    meta: { subtype: "YN" }
+    meta: { subtype: "YN" },
+    points: 1
   };
 };
