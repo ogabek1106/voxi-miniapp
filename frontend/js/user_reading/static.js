6372ca7e4fbe9ac01d2f00167cd4d947cf3a1889
ogabek1106 <84424884+ogabek1106@users.noreply.github.com>
2026-04-12 13:47:13 +0300

    Update static.js

--- frontend/js/user_reading/static.js
+++ frontend/js/user_reading/static.js
@@ -25,12 +25,12 @@ UserReading.renderShell = function (container) {
       ${UserReading.renderHeader()}
 
       <!-- DYNAMIC CONTENT -->
-      <div id="reading-user-content" style="
-        flex:1;
-        overflow-y:auto;
-        padding:0 12px;
-        box-sizing:border-box;
-      "></div>
+     <div id="reading-user-content" style="
+  flex:1;
+  overflow-y:auto;
+  padding:0 12px 70px 12px;
+  box-sizing:border-box;
+"></div>
 
     </div>
   `;
